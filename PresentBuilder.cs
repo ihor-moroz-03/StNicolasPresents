8f99294f9bdcca18858cd9541372e6c0b074093b
Ihor Moroz <ihor.moroz.17@gmail.com>
2021-11-10 18:31:31 +0200

    unnecessary usings cleared

--- PresentBuilder.cs
+++ PresentBuilder.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace StNicolasPresents
+﻿namespace StNicolasPresents
 {
     sealed class PresentBuilder : IPresentBuilder
     {
